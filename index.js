5a01edb1bdad070d170f439797a145a99e101a02
kmiscia <kmiscia@telvue.com>
2014-05-17 20:09:36 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  Board:  require('./lib/board'),
+  Led:    require('./lib/led'),
+  Piezo:  require('./lib/piezo'),
+  Button: require('./lib/button'),
+  Servo:  require('./lib/servo'),
+  Sensor: require('./lib/sensor'),
+  Ping:   require('./lib/ping'),
+  PIR:    require('./lib/pir'),
+  RC:     require('./lib/rc'),
+  IR:     require('./lib/ir')
+};
